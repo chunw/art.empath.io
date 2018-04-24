e56c5123ca017a46ae8369b1d857ca53cfdcd54a
Chun Wang <chunwang.me@gmail.com>
2018-04-23 17:18:57 -0700

    Add new homepage

--- main.py
+++ main.py
@@ -3,24 +3,22 @@ from flask import Flask, render_template, request, redirect
 import pymongo
 from pymongo import MongoClient
 
-#MONGO_URL = os.environ.get('MONGOHQ_URL')
-MONGO_URL = "mongodb://heroku_x9wjh6t4:fn8rhjvkf83rbjkjaeqn62igjr@ds127982.mlab.com:27982/heroku_x9wjh6t4"
-client = MongoClient(MONGO_URL)
+MONGO_URI = "mongodb://heroku_x9wjh6t4:fn8rhjvkf83rbjkjaeqn62igjr@ds127982.mlab.com:27982/heroku_x9wjh6t4"
+client = MongoClient(MONGO_URI)
 db = client['heroku_x9wjh6t4']
-
-#db = client.heroku_x9wjh6t4
-#database_user = "heroku_x9wjh6t4"
-#database_pass = "fn8rhjvkf83rbjkjaeqn62igjr"
-#db.authenticate(database_user, database_pass)
 collection = db.shoutouts
 
 app = Flask(__name__)
-#app = Flask(name)
 
 @app.route("/", methods=['GET'])
 def index():
     shouts = collection.find()
     return render_template('index.html', shouts=shouts)
+
+@app.route("/all", methods=['GET'])
+def all():
+    shouts = collection.find()
+    return render_template('all.html', shouts=shouts)
 
 @app.route("/post", methods=['POST'])
 def post():

876b70d0597566c3042eacb94e6451a6ebf9e516
Chun Wang <chunwang.me@gmail.com>
2018-05-10 09:34:12 -0700

    Cleanup codebase

--- node-printer/printer.js
+++ node-printer/printer.js
@@ -1,20 +1,9 @@
 /*
-The node.js module used to communicate with Adafruit/Sparkfun Thermal Printer:
-https://github.com/xseignard/thermalPrinter
-https://www.npmjs.com/package/thermalprinter
-https://itp.nyu.edu/physcomp/labs/labs-serial-communication/lab-serial-control-of-an-arduino/
-
-Example application: https://medium.com/offline-camp/how-to-fall-in-love-with-receipts-cd383ef31c3c
-https://iotfall2015.wordpress.com/tag/node-js/
-https://stackoverflow.com/questions/42560154/node-js-thermal-printer-issue-in-loop-printing
-
-Alternative pyduino (much more manual coding needed) https://github.com/lekum/pyduino/blob/master/pyduino_sketch.ino
+* The node.js module used to communicate with Adafruit Thermal Printer.
 */
-
 var SerialPort = require('serialport'),
     serialPort = new SerialPort('/dev/cu.usbmodem14241', {
   	   baudRate: 19200
-        //parser: SerialPort.parsers.readline("\n")
   	}),
 Printer = require('thermalprinter');
 http = require('http');
